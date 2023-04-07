39f95fedbe502906091d4a36cc3ecffd3c9119a0
Isabela <belarbarbosa@gmail.com>
2023-04-07 16:07:45 -0300

    Ajuste

--- src/infrastructure/database.js
+++ src/infrastructure/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const uri = `mongodb+srv://isabela:senha123@cluster0.hgbrisq.mongodb.net/?retryWrites=true&w=majority`;
+// substituir pelo seu link
+const uri = `mongodb+srv://user:<password>@cluster0.hgbrisq.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose.set("strictQuery", true);
 
